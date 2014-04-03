86aa5cb9ac832599d4ee04e168c348e79bc37e53
Dennis Schmidt <Dennis@Schmiddi-TP.(none)>
2014-04-03 10:57:17 +0200

    Rename SearchResult add new Search Result

---
+++ lucene/src/org/apache/lucene/GeneDocument.java
@@ -0,0 +1,54 @@
+package org.apache.lucene;
+
+public class GeneDocument {
+	private String source;
+	private String title;
+	private String author;
+	private String origin1;
+	private String origin2;
+	private String content;
+	private int PMID;
+	
+	public String getSource() {
+		return source;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public String getOrigin1() {
+		return origin1;
+	}
+	public String getOrigin2() {
+		return origin2;
+	}
+	public String getContent() {
+		return content;
+	}
+	public int getPMID() {
+		return PMID;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setOrigin1(String origin1) {
+		this.origin1 = origin1;
+	}
+	public void setOrigin2(String origin2) {
+		this.origin2 = origin2;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setPMID(int pMID) {
+		PMID = pMID;
+	}	
+}
--- lucene/src/org/apache/lucene/SearchResult.java
+++ lucene/src/org/apache/lucene/SearchResult.java
@@ -1,80 +1,24 @@
 package org.apache.lucene;
 
+import java.util.List;
+
 public class SearchResult {
+	private List<GeneDocument> documents;
+	private int totalHits;
 
-	private String source;
-	private String title;
-	private String author;
-	private String origin1;
-	private String origin2;
-	private String content;
-	private int PMID;
-
-	public SearchResult(String source, String title, String author,
-			String origin1, String origin2, String content, int pMID) {
-		super();
-		this.source = source;
-		this.title = title;
-		this.author = author;
-		this.origin1 = origin1;
-		this.origin2 = origin2;
-		this.content = content;
-		PMID = pMID;
+	public List<GeneDocument> getDocuments() {
+		return documents;
 	}
 
-	public String getSource() {
-		return source;
+	public int getTotalHits() {
+		return totalHits;
 	}
 
-	public String getTitle() {
-		return title;
+	public void setDocuments(List<GeneDocument> documents) {
+		this.documents = documents;
 	}
 
-	public String getAuthor() {
-		return author;
-	}
-
-	public String getOrigin1() {
-		return origin1;
-	}
-
-	public String getOrigin2() {
-		return origin2;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public int getPMID() {
-		return PMID;
-	}
-
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public void setOrigin1(String origin1) {
-		this.origin1 = origin1;
-	}
-
-	public void setOrigin2(String origin2) {
-		this.origin2 = origin2;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public void setPMID(int pMID) {
-		PMID = pMID;
+	public void setTotalHits(int totalHits) {
+		this.totalHits = totalHits;
 	}
 }

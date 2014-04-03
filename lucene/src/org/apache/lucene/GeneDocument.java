c561101a0cdc44d832918ae8fb7feb9b4daffc79
Kevin Kepp <dev@kevinkepp.de>
2014-04-03 10:59:28 +0200

    Add constructor to GeneDocument

--- lucene/src/org/apache/lucene/GeneDocument.java
+++ lucene/src/org/apache/lucene/GeneDocument.java
@@ -1,6 +1,7 @@
 package org.apache.lucene;
 
 public class GeneDocument {
+
 	private String source;
 	private String title;
 	private String author;
@@ -8,47 +9,72 @@ public class GeneDocument {
 	private String origin2;
 	private String content;
 	private int PMID;
-	
+
+	public GeneDocument(String source, String title, String author,
+			String origin1, String origin2, String content, int PMID) {
+		super();
+		this.source = source;
+		this.title = title;
+		this.author = author;
+		this.origin1 = origin1;
+		this.origin2 = origin2;
+		this.content = content;
+		this.PMID = PMID;
+	}
+
 	public String getSource() {
 		return source;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public String getOrigin1() {
 		return origin1;
 	}
+
 	public String getOrigin2() {
 		return origin2;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public int getPMID() {
 		return PMID;
 	}
+
 	public void setSource(String source) {
 		this.source = source;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public void setOrigin1(String origin1) {
 		this.origin1 = origin1;
 	}
+
 	public void setOrigin2(String origin2) {
 		this.origin2 = origin2;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	public void setPMID(int pMID) {
-		PMID = pMID;
-	}	
+
+	public void setPMID(int PMID) {
+		this.PMID = PMID;
+	}
 }

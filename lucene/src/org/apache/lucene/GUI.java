68dd7ccd0f589befd8e9d61afe4fc0cfac4f9d21
Kevin Kepp <dev@kevinkepp.de>
2014-04-03 14:31:29 +0200

    Fertisch

--- lucene/src/org/apache/lucene/GUI.java
+++ lucene/src/org/apache/lucene/GUI.java
@@ -2,6 +2,7 @@ package org.apache.lucene;
 
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -9,46 +10,33 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JComboBox;
-import javax.swing.JSplitPane;
-import javax.swing.JList;
-import javax.swing.border.LineBorder;
-import javax.swing.event.ListSelectionEvent;
-
-import java.awt.Color;
-
-import javax.swing.JToolBar;
-
-import java.awt.Component;
-
-import javax.swing.Box;
-
-import java.awt.GridLayout;
-
 import javax.swing.JSeparator;
-
-import com.sun.xml.internal.bind.v2.runtime.reflect.ListTransducedAccessorImpl;
-import java.awt.FlowLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 @SuppressWarnings("serial")
-public class GUI extends JFrame {
+public class GUI extends JFrame implements ListSelectionListener {
 
-	private static final String FIELD_ALL = "All";
 	private SearchInterface search;
 	private JPanel contentPane;
 	private JTextField txtSearch;
-	private JTextField txtTotalNo;
 	private JComboBox<String> comboBoxFields;
 	private JSplitPane splitPane;
 	private JList<String> listResults;
@@ -58,29 +46,14 @@ public class GUI extends JFrame {
 	private JButton btnNewButton;
 	private JSeparator separator;
 	private JPanel emptyPane;
-	private JLabel lblTotalNo;
-	private JLabel lblField;
-	private JPanel panel_1;
+	private SearchResult searchResult;
+	private List<GeneDocument> documentIndex;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GUI frame = new GUI(new SearchInterface() {
-
-						@Override
-						public SearchResult search(String query,
-								int startResult, int numberOfResults,
-								Fieldname fieldname) {
-							return null;
-						}
-
-						@Override
-						public SearchResult search(String query,
-								int startResult, int numberOfResults) {
-							return null;
-						}
-					});
+					GUI frame = new GUI(new MockSearch());
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -101,20 +74,14 @@ public class GUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 
-		// DocumentPanel d = new DocumentPanel(new GeneDocument("blub", "blub",
-		// "blub", "blub", "blub", "blub", 1));
-		// contentPane.add(d, new GridBagConstraints(0, 0, 1, 1, 0, 0,
-		// GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
-		// new Insets(0, 0, 0, 0), 0, 0));
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 1.0 };
-		gbl_contentPane.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0 };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, 0.0, 0.0 };
 		contentPane.setLayout(gbl_contentPane);
 
 		txtSearch = new JTextField();
 		txtSearch.setToolTipText("Search query");
 		GridBagConstraints gbc_txtSearch = new GridBagConstraints();
-		gbc_txtSearch.gridwidth = 3;
 		gbc_txtSearch.ipady = 7;
 		gbc_txtSearch.weightx = 1.0;
 		gbc_txtSearch.insets = new Insets(0, 0, 5, 5);
@@ -135,63 +102,41 @@ public class GUI extends JFrame {
 		GridBagConstraints gbc_btnSearch = new GridBagConstraints();
 		gbc_btnSearch.insets = new Insets(0, 0, 5, 0);
 		gbc_btnSearch.anchor = GridBagConstraints.NORTHWEST;
-		gbc_btnSearch.gridx = 3;
+		gbc_btnSearch.gridx = 2;
 		gbc_btnSearch.gridy = 0;
 		contentPane.add(btnSearch, gbc_btnSearch);
 
 		Vector<String> comboItems = new Vector<String>();
-		comboItems.add(FIELD_ALL);
+		comboItems.add("All fields");
 		for (Fieldname f : Fieldname.values()) {
 			comboItems.add(f.name());
 		}
-		
-		panel_1 = new JPanel();
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.insets = new Insets(5, 0, 8, 0);
-		gbc_panel_1.anchor = GridBagConstraints.NORTHWEST;
-		gbc_panel_1.gridwidth = 4;
-		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 1;
-		contentPane.add(panel_1, gbc_panel_1);
-		panel_1.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 0));
-		
-		lblTotalNo = new JLabel("Max. number of results");
-		panel_1.add(lblTotalNo);
-		
-				txtTotalNo = new JTextField();
-				txtTotalNo.setHorizontalAlignment(SwingConstants.RIGHT);
-				panel_1.add(txtTotalNo);
-				txtTotalNo.setText("20");
-				txtTotalNo.setColumns(5);
-		
-		lblField = new JLabel("Field");
-		panel_1.add(lblField);
 		comboBoxFields = new JComboBox<String>(comboItems);
-		panel_1.add(comboBoxFields);
+		GridBagConstraints gbc_comboBoxFields = new GridBagConstraints();
+		gbc_comboBoxFields.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBoxFields.gridx = 1;
+		gbc_comboBoxFields.gridy = 0;
+		contentPane.add(comboBoxFields, gbc_comboBoxFields);
 
 		splitPane = new JSplitPane();
 		splitPane.setOneTouchExpandable(true);
 		GridBagConstraints gbc_splitPane = new GridBagConstraints();
 		gbc_splitPane.anchor = GridBagConstraints.NORTHWEST;
-		gbc_splitPane.gridwidth = 4;
+		gbc_splitPane.gridwidth = 3;
 		gbc_splitPane.fill = GridBagConstraints.BOTH;
 		gbc_splitPane.gridx = 0;
-		gbc_splitPane.gridy = 2;
+		gbc_splitPane.gridy = 3;
 		contentPane.add(splitPane, gbc_splitPane);
 
 		panel = new JPanel();
 		splitPane.setLeftComponent(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 1, 26, 26, 73, 1, 0 };
-		gbl_panel.rowHeights = new int[] { 25, 0, 0, 0 };
-		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0,
-				Double.MIN_VALUE };
-		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
 
 		listResults = new JList<String>();
+		listResults.addListSelectionListener(this);
 		GridBagConstraints gbc_listResults = new GridBagConstraints();
+		gbc_listResults.weighty = 1.0;
 		gbc_listResults.fill = GridBagConstraints.BOTH;
 		gbc_listResults.gridwidth = 3;
 		gbc_listResults.anchor = GridBagConstraints.NORTHWEST;
@@ -200,32 +145,42 @@ public class GUI extends JFrame {
 		panel.add(listResults, gbc_listResults);
 
 		txtPage = new JTextField();
+		txtPage.setHorizontalAlignment(SwingConstants.RIGHT);
 		txtPage.setToolTipText("Page");
 		GridBagConstraints gbc_txtPage = new GridBagConstraints();
+		gbc_txtPage.weightx = 0.3;
+		gbc_txtPage.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtPage.ipady = 7;
-		gbc_txtPage.anchor = GridBagConstraints.WEST;
-		gbc_txtPage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtPage.insets = new Insets(0, 0, 0, 1);
 		gbc_txtPage.gridx = 0;
 		gbc_txtPage.gridy = 0;
 		panel.add(txtPage, gbc_txtPage);
 		txtPage.setText("1");
-		txtPage.setColumns(2);
+		txtPage.setColumns(4);
 
 		txtResultsPerPage = new JTextField();
+		txtResultsPerPage.setHorizontalAlignment(SwingConstants.RIGHT);
 		txtResultsPerPage.setToolTipText("Results Per Page");
 		GridBagConstraints gbc_txtResultsPerPage = new GridBagConstraints();
+		gbc_txtResultsPerPage.weightx = 0.3;
+		gbc_txtResultsPerPage.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtResultsPerPage.ipady = 7;
-		gbc_txtResultsPerPage.anchor = GridBagConstraints.WEST;
-		gbc_txtResultsPerPage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtResultsPerPage.insets = new Insets(0, 0, 0, 1);
 		gbc_txtResultsPerPage.gridx = 1;
 		gbc_txtResultsPerPage.gridy = 0;
 		panel.add(txtResultsPerPage, gbc_txtResultsPerPage);
 		txtResultsPerPage.setText("15");
-		txtResultsPerPage.setColumns(2);
+		txtResultsPerPage.setColumns(4);
 
 		btnNewButton = new JButton("Apply");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				startSearch();
+			}
+		});
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton.weightx = 0.3;
+		gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnNewButton.gridx = 2;
 		gbc_btnNewButton.gridy = 0;
 		panel.add(btnNewButton, gbc_btnNewButton);
@@ -233,7 +188,6 @@ public class GUI extends JFrame {
 		separator = new JSeparator();
 		GridBagConstraints gbc_separator = new GridBagConstraints();
 		gbc_separator.weightx = 1.0;
-		gbc_separator.insets = new Insets(3, 0, 3, 0);
 		gbc_separator.fill = GridBagConstraints.HORIZONTAL;
 		gbc_separator.gridwidth = 3;
 		gbc_separator.anchor = GridBagConstraints.NORTHWEST;
@@ -245,20 +199,32 @@ public class GUI extends JFrame {
 		splitPane.setRightComponent(emptyPane);
 	}
 
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		int index = listResults.getSelectedIndex();
+		JPanel p = emptyPane;
+		if (index != -1) {
+			GeneDocument doc = documentIndex.get(index);
+			if (doc != null) {
+				p = new DocumentPanel(doc);
+			}
+		}
+		splitPane.setRightComponent(p);
+	}
+
 	private void startSearch() {
 		String query = txtSearch.getText();
-		int numberOfResults = Integer.parseInt(txtTotalNo.getText());
 		int fieldIndex = comboBoxFields.getSelectedIndex();
 
 		SearchResult result;
 		if (fieldIndex == 0) {
 			// all
-			result = search.search(query, 1, numberOfResults);
+			result = search.search(query, getStartIndex(), getResultsPerPage());
 		} else {
 			Fieldname field = Fieldname.values()[fieldIndex - 1];
-			result = search.search(query, 1, numberOfResults, field);
+			result = search.search(query, getStartIndex(), getResultsPerPage(),
+					field);
 		}
-
 		updateSearchResults(result);
 	}
 
@@ -268,19 +234,28 @@ public class GUI extends JFrame {
 					"Error", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
-		List<DocumentPanel> docPanels = new ArrayList<DocumentPanel>();
+		searchResult = result;
+		documentIndex = new ArrayList<GeneDocument>();
 		List<String> titles = new Vector<String>();
 		for (GeneDocument geneDoc : result.getDocuments()) {
 			titles.add(geneDoc.getTitle());
-			docPanels.add(new DocumentPanel(geneDoc));
+			documentIndex.add(geneDoc);
 		}
 		// TODO sort list?
-		int page = Integer.parseInt(txtPage.getText());
-		int resultsPerPage = Integer.parseInt(txtResultsPerPage.getText());
-		int startIndex = (page - 1) * resultsPerPage + 1;
-		int endIndex = startIndex + resultsPerPage;
-		titles = titles.subList(startIndex, endIndex);
 		listResults.setListData(new Vector<String>(titles));
+	}
+
+	private int getCurrentPage() {
+		return Integer.parseInt(txtPage.getText());
+	}
+
+	private int getResultsPerPage() {
+		return Integer.parseInt(txtResultsPerPage.getText());
+	}
+
+	private int getStartIndex() {
+		int startIndex = (getCurrentPage() - 1) * getResultsPerPage() + 1;
+		return startIndex;
 	}
 
 	class DocumentPanel extends JPanel {
